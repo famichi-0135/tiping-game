7b756be2cca5168977d92559072c7b431550fc3c
tomo <tomop0513.maey@gmail.com>
2025-02-19 17:38:20 +0900

    正答率の実装、CSSでデザイン整理、サウンドの追加

--- main.js
+++ main.js
@@ -1,40 +1,35 @@
 {
-  // window.addEventListener('click',()=>{
-  //   const startDisplay = document.querySelector('.startDisplay');
-  //   startDisplay.classList.remove('startDisplay');
-  //   startDisplay.classList.add('startDisplayNone');
-  //   const tipingGame = document.querySelector('.tipingGame');
-  //   tipingGame.classList.remove();
-  //   tipingGame.classList.add('tipingGameScreen');
-  //   // tipingSistem();
-  // },{once: true});
-
+  
   function startToTaiping(){
-    const sbutton = document.querySelector('#startDisplay > button');
+    const sbutton = document.querySelector('#startDisplay > div > button');
     sbutton.addEventListener('click',() =>{
     //checkSpell()
        corectNo = 0;
        digitNo = 0;
        questonD = '';
+       missed = false;
+       wrongDigit = 0;
+       allDigit = 0;
       tipingSistem(corectNo);
       startToTaipingGame();
     });
   }
   startToTaiping();
-  // function oneClick(){
-  //   window.addEventListener('click',()=>{
-  //     startToTaipingGame();
-  //   },{once: true});
-  // }
-
-
-  //１ゲームの出題問題数
-  const quesitonNo = 2;
-
+  
+  //ゲームの出題問題数
+  const quesitonNo = 5;
+  // 現在の問題番号
   let corectNo = 0;
+  //自分が今何文字打ったかをカウントする変数
   let digitNo = 0;
-
+  //現在の出題単語の文字数記録変数
   let questonD = [];
+  //問題全ての文字数
+  let allDigit = 0;
+  //不正確なタイピング記録変数
+  let wrongDigit = 0;
+  //一回目のミスのみを記録するためのブール関数的な何か
+  let missed = false;
 
   const tipingDocument =[
     {english:'yakkyoku', japanese:'薬局',},
@@ -43,8 +38,14 @@
     {english:'erefanntokasimasi', japanese:'エレファントカシマシ',},
     {english:'toukyouerekutoronn', japanese:'東京エレクトロン',},
     {english:'sisumekkusu', japanese:'シスメックス',},
-    {english:'bandainamuko', japanese:'バンダイナムコ',},
+    {english:'banndainamuko', japanese:'バンダイナムコ',},
     {english:'sukueaenikkusu', japanese:'スクエアエニックス',},
+    {english:'ko-e-tekumo', japanese:'コーエーテクモ',},
+    {english:'furomusofutowea', japanese:'フロムソフトウェア',},
+    {english:'kapukonn', japanese:'カプコン',},
+    {english:'nihonnseitetu', japanese:'日本製鉄',},
+    {english:'saiba-e-zyento', japanese:'サイバーエージェント',},
+    {english:'suteranntelisu', japanese:'ステンランティス',},
   ];
 
 
@@ -55,9 +56,7 @@
         checkSpell();
       }else{
         tipingGameToFinishDisplay();
-      //  corectNo = 0;
-      //  digitNo = 0;
-      //  questonD = '';
+        console.log(Math.floor((allDigit - wrongDigit) / allDigit));
        switchFinishButton();
         // return;
       }
@@ -88,8 +87,10 @@
         tipingGame.classList.remove('tipingGameScreen');
         tipingGame.classList.add('tipingGame');
         let finishScreen = document.querySelector('#finalDisplay');
+        let finishScreenP = document.querySelector('#finalDisplay  p');
         finishScreen.classList.remove('finalDisplay');
         finishScreen.classList.add('finalDisplayScreen');
+        finishScreenP.textContent = `${Math.floor(((allDigit - wrongDigit) / allDigit) * 100)}%`;
     }
 
     function FinishDisplayToStartDisplay(){
@@ -114,6 +115,8 @@
     let randam = Math.floor(Math.random() * tipingDocument.length);
     let documentSlice = [];
     const english = tipingDocument[randam].english;
+    //文字数数え上げ変数
+    allDigit = allDigit + english.length;
     for(let i = 0; i < tipingDocument[randam].english.length; i++){
       documentSlice = english.slice(0,tipingDocument[randam].english.length);
       let span = document.createElement('span');
@@ -136,17 +139,28 @@
   }
   
 
-  
-
   function keydownHandler(event){
-    // let questonD = editSpell();
+    const audioTipe = new Audio('タイピング-メカニカル単2.mp3');
+    const audioMiss = new Audio('エラー2.mp3');
+    const audioOK = new Audio('正解4.mp3');
+
+    audioMiss.volume = .1;
+    audioTipe.volume = .5;
+    audioOK.volume = .2;
+    let wdigit = 0;
     if(questonD[digitNo] === event.key){
       console.log("corect");
+      audioTipe.currentTime = 0;
+      audioTipe.play();
       let span = document.querySelector(`.color${digitNo}`);
+      span.classList.remove('addcolorWrong');
       span.classList.add('addcolor');
+      missed = false;
       digitNo++;
       if(digitNo === questonD.length){
         console.log('All corect');
+        audioOK.currentTime = 0;
+        audioOK.play();
         digitNo = 0;
         corectNo++;
         for(let i = 0; i < questonD.length;i++){
@@ -154,10 +168,19 @@
           span.remove();
         }
         questonD = '';
-
+        missed = false;
         tipingSistem(corectNo);
       }
     }else if(questonD[digitNo] !== event.key){
+      audioMiss.currentTime = 0;
+      audioMiss.play();
+      if(missed === false){
+        wdigit++;
+        wrongDigit +=  wdigit;
+        missed = true;
+      }
+      let span = document.querySelector(`.color${digitNo}`);
+      span.classList.add('addcolorWrong');
       console.log('wrong');
     }
   }
